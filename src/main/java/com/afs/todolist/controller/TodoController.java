525f03badac93422f22b41d8c6143b47ef242b3f
tokaho <tokaho2000@gmail.com>
2022-12-15 12:01:04 +0800

    [Feat] Finshed get post put delete api, Allowed CORS request from localhost:3001 on all action

--- src/main/java/com/afs/todolist/controller/TodoController.java
+++ src/main/java/com/afs/todolist/controller/TodoController.java
@@ -1,11 +1,11 @@
 package com.afs.todolist.controller;
 
+import com.afs.todolist.controller.dto.TodoCreateRequest;
+import com.afs.todolist.controller.dto.TodoUpdateRequest;
 import com.afs.todolist.controller.mapper.TodoMapper;
 import com.afs.todolist.entity.Todo;
 import com.afs.todolist.service.TodoService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +25,21 @@ public class TodoController {
     List<Todo> getAll() {
         return todoService.findAll();
     }
+
+    @PostMapping
+    Todo createTodo(@RequestBody TodoCreateRequest todoCreateRequest) {
+        Todo newTodoObj = todoMapper.toEntity(todoCreateRequest);
+        return todoService.createTodo(newTodoObj);
+    }
+
+    @DeleteMapping("/{id}")
+    void deleteTodo(@PathVariable String id) {
+        todoService.deleteTodo(id);
+    }
+
+    @PutMapping("/{id}")
+    Todo updateTodo(@PathVariable String id, @RequestBody TodoUpdateRequest todoUpdateRequest) {
+        return todoService.updateTodo(id, todoUpdateRequest);
+    }
+
 }
--- src/main/java/com/afs/todolist/service/TodoService.java
+++ src/main/java/com/afs/todolist/service/TodoService.java
@@ -1,11 +1,13 @@
 package com.afs.todolist.service;
 
+import com.afs.todolist.controller.dto.TodoUpdateRequest;
 import com.afs.todolist.entity.Todo;
 import com.afs.todolist.exception.InvalidIdException;
 import com.afs.todolist.repository.TodoRepository;
 import org.bson.types.ObjectId;
 import org.springframework.stereotype.Service;
 
+import javax.xml.ws.http.HTTPException;
 import java.util.List;
 
 @Service
@@ -21,9 +23,30 @@ public class TodoService {
         return todoRepository.findAll();
     }
 
-    private void validateObjectId(String id){
-        if(!ObjectId.isValid(id)){
+    private void validateObjectId(String id) {
+        if (!ObjectId.isValid(id)) {
             throw new InvalidIdException(id);
         }
     }
+
+    public Todo createTodo(Todo newTodoObj) {
+        return todoRepository.save((newTodoObj));
+    }
+
+    public void deleteTodo(String id) {
+        todoRepository.deleteById(id);
+    }
+
+    public Todo updateTodo(String id, TodoUpdateRequest todoUpdateRequest) {
+        Todo existingTodo = todoRepository.findById(id).get();
+        if (todoUpdateRequest.getDone() != null) {
+            existingTodo.setDone(todoUpdateRequest.getDone());
+        }
+        if (todoUpdateRequest.getText() != null) {
+            existingTodo.setText(todoUpdateRequest.getText());
+        }
+        return todoRepository.save(existingTodo);
+    }
+
+
 }
